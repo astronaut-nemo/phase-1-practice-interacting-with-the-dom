90dec749ad85c74874666bd4fa97a778f357592b
Joanna <astronautnemo@gmail.com>
2022-12-02 09:45:48 +0300

    added second deliverable (plus and minus)

--- js/challenge.js
+++ js/challenge.js
@@ -4,4 +4,15 @@ document.addEventListener('DOMContentLoaded', () => {
     let counter = document.getElementById('counter');
     setInterval(() => {++counter.textContent}, 1000); /* Increase counter every one second */
 
+    // 2. Manually increment and decrement the counter using the plus and minus buttons.
+    // Minus button
+    let minusBtn = document.getElementById('minus');
+    minusBtn.addEventListener('click', () => {
+        --counter.textContent
+    })
+    // Plus button
+    let plusBtn = document.getElementById('plus');
+    plusBtn.addEventListener('click', () => {
+        ++counter.textContent
+    })
 })
